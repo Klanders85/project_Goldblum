29628ce2478a60b73199b792f82ee9dd5dee3203
Kelly Landers <kelly.landers@sq1.com>
2015-02-16 21:17:39 -0600

    stopping point

--- js/main.js
+++ js/main.js
@@ -29,6 +29,13 @@
 			"year"  : "1997",
 			"genre" : "Action / Adventure"
 		}
+	},
+	{	
+		"tv_show" : {
+			"title" : "The League (TV Show) : Ruxin&rsquo;s Dad",
+			"year"  : "2010",
+			"genre" : "TV Show / Comedy"
+		}
 	}];
 
 $(document).ready(function(){
@@ -36,7 +43,15 @@ $(document).ready(function(){
 		var br = '<br />';
 		var movieTitle =  this.movie.title;
 		var movieYear  =  this.movie.year;
-		var movieGenre  =  this.movie.genre;
+		var movieGenre =  this.movie.genre;
 		$('#film-grid').append('<p>' + movieTitle + '  ' + movieYear + '  ' + movieGenre + '</p>');
 	})
+
+	// $.each(filmography, function(arrayIndex, value){
+	// 	var br = '<br />';
+	// 	var tv_showTitle =  this.tv_show.title;
+	// 	var tv_showYear  =  this.tv_show.year;
+	// 	var tv_showGenre =  this.tv_show.genre;
+	// 	$('#film-grid').append('<p>' + tv_showTitle + '  ' + tv_showYear + '  ' + tv_showGenre + '</p>');
+	// })
 })

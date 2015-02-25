39cddd5c1678b0feb253ef4058515a7d54a3681f
Kelly Landers <kelly.landers@sq1.com>
2015-02-25 16:42:59 -0600

    integrating into Angular app for filmography

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
-// create a grid using a loop that will print out divs for each object from API response
-
-// create a simple object
+(function(){
+	var app = angular.module('filmography', [])
+	
 	var filmography = [
-	{
-		"movie" : {
-			"title" : "Jurrasic Park",
-			"year"  : "1993",
-			"genre" : "Action / Adventure"
+		{
+			"movie" : {
+				"title" : "Jurrasic Park",
+				"year"  : "1993",
+				"genre" : "Action / Adventure"
+			}
+		},
+		{
+			"movie" : {
+				"title" : "Independence Day",
+				"year"  : "1996",
+				"genre" : "Action / Adventure"
+			}
+		},
+		{
+			"movie" : {
+				"title" : "The Fly",
+				"year"  : "1986",
+				"genre" : "Sci - Fi / Gross"
+			}
+		},
+		{	
+			"movie" : {
+				"title" : "Lost World : Jurrasic Park 2",
+				"year"  : "1997",
+				"genre" : "Action / Adventure"
+			}
+		},
+		{	
+			"tv_show" : {
+				"title" : "The League (TV Show) : Ruxin's Dad",
+				"year"  : "2010",
+				"genre" : "TV Show / Comedy"
+			}
 		}
-	},
-	{
-		"movie" : {
-			"title" : "Independence Day",
-			"year"  : "1996",
-			"genre" : "Action / Adventure"
-		}
-	},
-	{
-		"movie" : {
-			"title" : "The Fly",
-			"year"  : "1986",
-			"genre" : "Sci - Fi / Gross"
-		}
-	},
-	{	
-		"movie" : {
-			"title" : "Lost World : Jurrasic Park 2",
-			"year"  : "1997",
-			"genre" : "Action / Adventure"
-		}
-	},
-	{	
-		"tv_show" : {
-			"title" : "The League (TV Show) : Ruxin's Dad",
-			"year"  : "2010",
-			"genre" : "TV Show / Comedy"
-		}
-	}];
+	]; //end filmography array
 
 
-$(document).ready(function(){
-	$.each(filmography, function(){
-		$.each(this, function(index,value){
-			// $('#film-grid').append('<p>' + index + '</p>');
-			$.each(this, function(name, value){
-				$('#film-grid').append('<h4>'+ value +'</h4>');
-			})
-		})
+	app.controller('FilmController', function(){
+		this.movie = filmography;
 	})
-})
+
+})(); // end closure
+
+
+
+
+

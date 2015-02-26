7744ce14aa633988ca0c6952166e945dbfaff7c6
Kelly Landers <kelly.landers@sq1.com>
2015-02-26 15:11:28 -0600

    add more angular functionality to filmo page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 (function(){
-	var app = angular.module('goldBlum', [])
+	var app     = angular.module('goldBlum', []);
 	
-	app.controller('FilmController', function(){
+	app.controller('FilmController', function($scope){
 		this.filmData = filmography;
 	})
+
 
 	var filmography = [
 		// {
@@ -18,41 +19,51 @@
 			year  : "1986",
 			genre : "Sci - Fi / Gross",
 			media : "Movie",
-			image : '/img/posters/theFly.jpg'
+			image : 'http://ia.media-imdb.com/images/M/MV5BMTQ5OTgwOTExM15BMl5BanBnXkFtZTcwMTE4ODE5OQ@@._V1_SY317_CR5,0,214,317_AL_.jpg',
+			imdbId : 'tt0091064',
+			showSummary : false,
+			summary : 'A brilliant but eccentric scientist begins to transform into a giant man/fly hybrid after one of his experiments goes horribly wrong.'
 		},
 		{
 			title : "Jurrasic Park",
 			year  : "1993",
 			genre : "Action / Adventure",
 			media : "Movie",
-			image : '/img/posters/jurassicPark.jpg'
+			image : 'http://ia.media-imdb.com/images/M/MV5BMjQzODQyMzk2Nl5BMl5BanBnXkFtZTcwNTg4MjQ3OA@@._V1_SX214_AL_.jpg',
+			imdbId  : 'tt0107290',
+			showSummary : false,
+			summary : 'During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.' 
 		},
 		{
 			title : "Independence Day",
 			year  : "1996",
 			genre : "Action / Adventure",
 			media : "Movie",
-			image : '/img/posters/independenceDay.jpg'
+			image : 'http://ia.media-imdb.com/images/M/MV5BMTMwODY3NzQzNF5BMl5BanBnXkFtZTcwNzUxNjc0MQ@@._V1_SY317_CR6,0,214,317_AL_.jpg',
+			imdbId  : 'tt0116629',
+			showSummary : false,
+			summary : 'The aliens are coming and their goal is to invade and destroy. Fighting superior technology, Man&rsquo;s best weapon is the will to survive.'
 		},
 		{	
-			title : "Lost World:Jurrasic Park",
+			title : "Lost World : Jurrasic Park",
 			year  : "1997",
 			genre : "Action / Adventure",
 			media : "Movie",
-			image : '/img/posters/lostWorld.jpg'
+			image : 'http://ia.media-imdb.com/images/M/MV5BMTYxNjY1NjE2OV5BMl5BanBnXkFtZTYwNzE0MDc4._V1_SX214_AL_.jpg',
+			imdbId  : 'tt0119567',
+			showSummary : false,
+			summary : 'A research team is sent to the Jurassic Park Site B island to study the dinosaurs there while another team approaches with another agenda.'
 		},
 		{	
 			title : "Grand Budapest Hotel",
 			year  : "2014",
 			genre : "Drama",
 			media : "Movie",
-			image : '/img/posters/grandBudapestHotel.jpg'
+			image : 'http://ia.media-imdb.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_SX214_AL_.jpg',
+			imdbId  : 'tt2278388',
+			showSummary : false,
+			summary : 'The adventures of Gustave H, a legendary concierge at a famous hotel from the fictional Republic of Zubrowka between the first and second World Wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.' 
 		}
 	]; //end filmography array
 
 })(); // end closure
-
-
-
-
-
